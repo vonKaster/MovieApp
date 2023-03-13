e41e156b736c0a7edf828a8ce4ab6a6354139f13
Fran <francocaminosmail@gmail.com>
2023-03-13 09:15:41 -0300

    navBar and Footer added && services and models folder added

---
+++ src/components/commons/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-footer
+    :dark="$vuetify.theme.dark"
+    color="#ffffff"
+    class="custom-footer elevation-3"
+    padless
+  >
+    <v-col class="text-center" cols="12">
+      {{ new Date().getFullYear() }} —
+      <strong
+        ><a
+          class="footerLink"
+          target="_blank"
+          href="https://github.com/vonKaster"
+          >vonKaster</a
+        >
+        (Front-End Rookie for Digicard)</strong
+      >
+    </v-col>
+  </v-footer>
+</template>
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style>
+.footerLink {
+  color: #e6105b !important;
+  text-decoration: none;
+}
+
+.theme--dark .v-footer {
+  background-color: #272727 !important;
+  color: white !important;
+}
+</style>
