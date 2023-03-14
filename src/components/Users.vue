c2152e36d4af155d2cf4fa8d853b4e7cb7588ee7
Fran <francocaminosmail@gmail.com>
2023-03-14 11:59:38 -0300

    createUser & editUser

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,21 +1,274 @@
 <template>
-  <div></div>
+  <div>
+    <v-container>
+      <div v-if="!isLoaded">
+        <v-row>
+          <v-col
+            v-for="usuario in getAllUsers"
+            :key="usuario.id"
+            cols="12"
+            md="6"
+            lg="4"
+          >
+            <v-card class="mt-4">
+              <v-skeleton-loader
+                height="64px"
+                width="371px"
+                type="image"
+              ></v-skeleton-loader>
+              <v-card-title v-if="isLoaded">{{ usuario.nombre }}</v-card-title>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+      <div v-if="isLoaded">
+        <v-row>
+          <v-col class="text-center">
+            <v-btn class="mt-4" text @click="openAddDialog()"
+              >Crear Usuario</v-btn
+            >
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col
+            v-for="usuario in getAllUsers"
+            :key="usuario.id"
+            cols="12"
+            md="6"
+            lg="4"
+          >
+            <v-card class="mt-4">
+              <div class="d-flex justify-space-between align-center">
+                <v-avatar class="ms-4"
+                  ><v-img width="48px" src="../assets/user.jpg"></v-img
+                ></v-avatar>
+                <v-card-title style="font-size: 16px" class="flex-grow-1">{{
+                  usuario.nombre
+                }}</v-card-title>
+                <v-btn x-small text @click="openEditDialog(usuario)"
+                  ><v-icon>mdi-pencil</v-icon></v-btn
+                >
+                <v-btn x-small text class="mr-2" @click="showUserDialog(usuario)"
+                  ><v-icon>mdi-more</v-icon></v-btn
+                >
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </div>
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-card-title>{{ selectedUser.nombre }}</v-card-title>
+          <v-card-text>
+            <div>
+              <v-icon class="mr-2">mdi-account</v-icon>Nombre de usuario:
+              {{ selectedUser.nombreUsuario }}
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">mdi-email</v-icon>Correo Electrónico:
+              {{ selectedUser.correo }}
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">mdi-cellphone</v-icon>Teléfono:
+              {{ selectedUser.telefono }}
+            </div>
+            <div class="mt-2">
+              <v-icon class="mr-2">mdi-web</v-icon>Sitio Web:
+              {{ selectedUser.sitioWeb }}
+            </div>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="#6247aa" text @click="dialog = false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog width="500px" v-model="editDialog" persistent>
+        <v-card class="text-center">
+          <v-form
+            @submit.prevent="
+              editingUser ? editUser(userData) : createUser(userData)
+            "
+          >
+            <v-card-title class="headline">
+              {{
+                editingUser
+                  ? "Editar Usuario [" + selectedUser.id + "]"
+                  : "Agregar Usuario"
+              }}
+            </v-card-title>
+            <h3>
+              <p>Nombre</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.nombre"
+              append-icon="mdi-rename-box"
+            ></v-text-field>
+
+            <h3>
+              <p>Usuario</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.nombreUsuario"
+              append-icon="mdi-file-document-edit"
+            ></v-text-field>
+
+            <h3>
+              <p>Correo Electrónico</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.correo"
+              append-icon="mdi-image-edit"
+            ></v-text-field>
+
+            <h3>
+              <p>Teléfono</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.telefono"
+              append-icon="mdi-image-edit"
+            ></v-text-field>
+
+            <h3>
+              <p>Sitio Web</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.sitioWeb"
+              append-icon="mdi-image-edit"
+            ></v-text-field>
+
+            <h3>
+              <p>Calle</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.direccion.calle"
+              append-icon="mdi-application-edit"
+            ></v-text-field>
+
+            <h3>
+              <p>Departamento</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.direccion.departamento"
+              append-icon="mdi-cash"
+            ></v-text-field>
+
+            <h3>
+              <p>Ciudad</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.direccion.ciudad"
+              append-icon="mdi-cash"
+            ></v-text-field>
+
+            <h3>
+              <p>Codigo Postal</p>
+            </h3>
+            <v-text-field
+              type="text"
+              style="width: 400px"
+              class="mx-auto"
+              dense
+              solo
+              v-model="userData.direccion.codigoPostal"
+              append-icon="mdi-cash"
+            ></v-text-field>
+
+            <v-card-actions>
+              <v-btn
+                text
+                @click="
+                  editDialog = false;
+                  if (editingUser) editingUser = false;
+                  resetUserData();
+                "
+                >Cancelar</v-btn
+              >
+              <v-btn type="submit" color="green" text>Confirmar</v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-dialog>
+    </v-container>
+  </div>
 </template>
 
 <script>
 import users from "../store/users";
+
 export default {
   name: "Users",
 
   data() {
-    return {};
+    return {
+      isLoaded: false,
+      dialog: false,
+      selectedUser: {},
+      editDialog: false,
+      userEdited: null,
+      userData: {
+        nombre: null,
+        nombreUsuario: null,
+        correo: null,
+        telefono: null,
+        sitioWeb: null,
+        direccion: {
+          calle: null,
+          departamento: null,
+          ciudad: null,
+          codigoPostal: null,
+        },
+      },
+      editingUser: false,
+    };
+  },
+
+  created() {
+    document.title = "Digicard | Usuarios";
   },
 
   async mounted() {
     try {
-      await Promise.all([
-        users.dispatch("getAllUsers"),
-      ]);
+      await Promise.all([users.dispatch("getAllUsers")]);
       this.isLoaded = true;
     } catch (error) {
       console.log(error);
@@ -28,6 +281,79 @@ export default {
     },
   },
 
-  methods: {},
+  methods: {
+    showUserDialog(user) {
+      this.selectedUser = user;
+      this.dialog = true;
+    },
+    openEditDialog(user) {
+      this.selectedUser = user;
+      this.editingUser = true;
+      this.editDialog = true;
+      this.userData = user;
+    },
+    openAddDialog() {
+      this.editDialog = true;
+    },
+    async createUser(data) {
+      console.log("DATA COMPONENTE", data);
+      this.editDialog = false;
+      await users.dispatch("createUser", data);
+      this.selectedUser = {};
+      this.resetUserData();
+    },
+    async editUser(data) {
+      this.editDialog = false;
+      console.log(data.id);
+      console.log("DATA COMPONENTE", data);
+      await users.dispatch("editUser", data);
+      this.selectedUser = {};
+      this.resetUserData();
+      this.editingProduct = false;
+    },
+    resetUserData() {
+      this.userData = {
+        nombre: null,
+        nombreUsuario: null,
+        correo: null,
+        telefono: null,
+        sitioWeb: null,
+        direccion: {
+          calle: null,
+          departamento: null,
+          ciudad: null,
+          codigoPostal: null,
+        },
+      };
+    }
+  },
 };
 </script>
+
+<style>
+.theme--dark .v-btn {
+  background-color: #1e1e1e !important;
+  color: white;
+}
+
+.theme--dark .v-btn:before {
+  background-color: #272727 !important;
+}
+
+.v-btn {
+  background-color: #ffffff !important;
+  color: black;
+}
+
+.v-btn:hover {
+  color: #6247aa;
+}
+
+.v-btn--active {
+  color: #6247aa !important;
+}
+
+.v-btn:before {
+  background-color: #ffffff !important;
+}
+</style>
--- src/store/users.js
+++ src/store/users.js
@@ -18,18 +18,76 @@ export default new Vuex.Store({
   },
   actions: {
     async getAllUsers(state) {
-        return new Promise(async (resolve, reject) => {
-          try {
-            let item = await state.state.service.getAllUsers();
-            console.log(item);
-  
-            state.commit("setAllUsers", item);
-            resolve(true);
-          } catch (error) {
-            reject(error);
-          }
-        });
-      },
+      return new Promise(async (resolve, reject) => {
+        try {
+          let item = await state.state.service.getAllUsers();
+          state.commit("setAllUsers", item);
+          resolve(true);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    },
+    async createUser(state, data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          let item = await state.state.service.createUser(data);
+          let allUsers = state.state.allUsers;
+          let newUser = {
+            nombre: item.data.name,
+            nombreUsuario: item.data.username,
+            correo: item.data.email,
+            telefono: item.data.phone,
+            sitioWeb: item.data.website,
+            direccion: {
+              calle: item.data.address.street,
+              departamento: item.data.address.suite,
+              ciudad: item.data.address.city,
+              codigoPostal: item.data.address.zipcode,
+            },
+          };
+          allUsers.push(newUser);
+          state.commit("setAllUsers", allUsers);
+          resolve(true);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    },
+
+    async editUser(state, data) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          console.log("DATA ID STORE:", data.id);
+          let item = await state.state.service.editUser(data);
+          let allUsers = state.state.allUsers;
+          console.log("ITEM EN STORE, LLAMADA:", item);
+          allUsers = allUsers.map((user) => {
+            if (user.id != data.id) {
+              return allUsers;
+            }
+            user = {
+              nombre: item.data.name,
+              nombreUsuario: item.data.username,
+              correo: item.data.email,
+              telefono: item.data.phone,
+              sitioWeb: item.data.website,
+              direccion: {
+                calle: item.data.address.street,
+                departamento: item.data.address.suite,
+                ciudad: item.data.address.city,
+                codigoPostal: item.data.address.zipcode,
+              },
+            };
+            return user;
+          });
+          state.commit("setAllUsers", allUsers);
+          resolve(true);
+        } catch (error) {
+          reject(error);
+        }
+      });
+    },
   },
   modules: {},
 });
